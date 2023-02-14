0972e69d97d7bce911550c378135b968716f9993
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-02-14 23:59:44 +1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <div className='outer-container'>
+        <div className='inner-container'>
+
+        </div>
+      </div>
+    </main>
   )
 }
 
