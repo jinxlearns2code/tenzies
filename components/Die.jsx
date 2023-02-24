7b7b4950d754abb6404548adadcf37615a07f46b
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-02-24 23:43:00 +1100

    changed die face

--- components/Die.jsx
+++ components/Die.jsx
@@ -8,7 +8,44 @@ export default function Die(props) {
 			style={props.isHeld ? { backgroundColor: "#59E391" } : { backgroundColor: "#FFFFFF" }}
 			onClick={props.handleClick}
 		>
-			<h2 className="die-face">{props.value}</h2>
+			<div>
+				{props.value === 1 && <div className="die 1"></div>}
+				{props.value === 2 && <div className="die-container second">
+					<div className="die"></div> 
+					<div className="die"></div>
+					</div>
+				}
+				{props.value === 3 && <div className="die-container third">
+					<div className="die"></div> 
+					<div className="die"></div>
+					<div className="die"></div>
+					</div>
+				}
+				{props.value === 4 && <div className="die-container fourth">
+					<div className="die"></div> 
+					<div className="die"></div>
+					<div className="die"></div>
+					<div className="die"></div>
+					</div>
+				}
+				{props.value === 5 && <div className="die-container fifth">
+					<div className="die one"></div> 
+					<div className="die two"></div>
+					<div className="die three"></div>
+					<div className="die four"></div>
+					<div className="die five"></div>
+					</div>
+				}
+				{props.value === 6 && <div className="die-container sixth">
+					<div className="die"></div> 
+					<div className="die"></div>
+					<div className="die"></div>
+					<div className="die"></div>
+					<div className="die"></div>
+					<div className="die"></div>
+					</div>
+				}
+			</div>
 		</div>	
 	)
 }
